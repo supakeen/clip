132fb3c01e82f6b8b3981c66f3f6cf4c741977d4
supakeen <cmdr@supakeen.com>
2020-10-23 21:05:52 +0200

    Add some comments.

--- src/clip.nim
+++ src/clip.nim
@@ -4,17 +4,28 @@ import sequtils
 import strutils
 
 proc unsafe(arg: TaintedString): bool =
-  find(arg, re"[^\w@%+=:,./-]") == -1
+  # See if any characters that should be quoted are in an argument
+  find(arg, re"[^\w@%+=:,./-]") != -1
 
 proc quote(arg: TaintedString): string =
+  # Quote a single argument. Empty strings get turned into '', strings that
+  # have unsafe characters in them get wrapped in single quotes and single
+  # quotes inside them put inside double quotes. If none of those apply the
+  # original argument is returned.
   if len(arg) == 0:
     "''"
   elif unsafe(arg):
-    arg.string
-  else:
     "'" & arg.replace("'", "\"'\"") & "'"
+  else:
+    arg.string
 
 proc reassemble(args: seq[TaintedString]): string =
+  # Turn the argv as given to us by ``commandLineParams()`` back into an
+  # escaped string. Note that this isn't necessarily the command line used to
+  # invoke the program.
   map(args, quote).join(" ")
 
-echo reassemble(commandLineParams())
+proc parse(args: seq[TaintedString]) =
+  echo reassemble(args)
+
+parse(commandLineParams())
